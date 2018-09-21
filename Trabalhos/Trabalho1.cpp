a3afefc12ed670ab9f749f4b648952569cb2e58b
Mateus Lima <43353113+immlima@users.noreply.github.com>
2018-09-21 18:05:03 -0300

    Add files via upload

---
+++ Trabalhos/Trabalho1.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+class Matriz{
+	private:
+		int L, C, **Matriz1;
+
+	public:
+		Matriz();
+		int GetL();
+		int GetC();
+		void ValLC();
+		void MatrizAssigner();
+		void MosVal();
+		void Desaloca();
+};
+void Matriz::ValLC(){
+	cout<<"Digite o número de linhas da matriz:"<<endl;
+	cin>>L;
+	this->L=L;
+	cout<<"Digite o numero de colunas da matriz:"<<endl;
+	cin>>C;
+	this->C=C;
+}
+int Matriz::GetL(){
+	return L;
+}
+int Matriz::GetC(){
+	return C;
+}
+Matriz::Matriz(){
+	int i;
+	int L1=GetL();
+	int C1=GetC();
+	int **Mat;
+	Mat= new int*[L1];
+	for(i=0;i<L1;i++){
+		Mat[i]= new int[C1];
+	}
+	Matriz1=Mat;
+}
+void Matriz::MatrizAssigner(){
+	int i,j;
+	int L1=GetL();
+	int C1=GetC();
+	for(i=0;i<L1;i++){
+		for(j=0;j<C1;j++){
+		Matriz1[i][j]=(i+1)*10+j+1;
+		}
+	}
+}
+void Matriz::Desaloca(){
+	int i;
+	int L1=GetL();
+	for(i=0;i<L1;i++){
+		delete[] Matriz1[i];
+	}
+}
+
+void Matriz::MosVal(){
+	int i,j;
+	int L1=GetL();
+	int C1=GetC();
+	for(i=0;i<L1;i++){
+		cout<<endl;
+		cout<<"|";
+        for(j=0;j<C1;j++){
+			cout<<" A"<<Matriz1[i][j];
+		}
+		cout<<"|";
+	}
+}
+void DarValor(Matriz *Mat){
+	Mat->ValLC();
+	Mat->MatrizAssigner();
+	Mat->MosVal();
+}
+int main(){
+	Matriz Mat;
+ 	DarValor(&Mat);
+    return 0;
+}
